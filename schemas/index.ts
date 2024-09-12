f5efa412d4b0aa5fb53cae6e2250feaa15fee20d
Jair Francisco <jairfrancisco2018@gmail.com>
2024-09-12 12:51:25 -0300

    feat: Register Forms

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
+
 export const LoginSchema = z.object({
   email: z.string().email({ message: "Email inválido!" }),
   password: z.string().min(3, { message: "Senha inválida!" }),
+});
+
+export const RegisterSchema = z.object({
+  name: z.string().min(1, { message: "Nome é obrigatório!" }),
+  email: z.string().email({ message: "Email inválido!" }),
+  password: z
+    .string()
+    .min(6, { message: "Senha deve ter no mínimo 6 caracteres!" }),
 });
