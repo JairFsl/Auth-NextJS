df19007033a51af723c98da4dcbf8330324af71a
Jair Francisco <jairfrancisco2018@gmail.com>
2024-10-22 15:28:48 -0300

    feat: Middleware authentication | Specific Routes description

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,29 @@
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
+// import Github from "next-auth/providers/github";
+// import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
 
 import type { NextAuthConfig } from "next-auth";
+import { getUserByEmail } from "./services/user";
 
 export default {
-  providers: [Github, Google],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,43 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "./routes";
+import { NextResponse } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+  const isApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
   console.log("ROUTE: ", req.nextUrl.pathname);
   console.log("isLoggedIn: ", isLoggedIn);
+  console.log("isApiRoute: ", isApiRoute);
+  console.log("isPublicRoute: ", isPublicRoute);
+  console.log("isAuthRoute: ", isAuthRoute);
+
+  if (isApiRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
 });
 
 export const config = {
